b011ab9cdc2945d90ee6515081ccbd85ffa62b86
Anvi Dalal <anvi@seas.upenn.edu>
2017-05-02 21:05:07 -0400

    gh-pages update

--- bundle.js
+++ bundle.js
@@ -59,10 +59,32 @@
 	var THREE = __webpack_require__(6);
 	
 	
-	var input = {
-	  layout: false,
-	  shapeGrammar: true
+	var startTime = Date.now();
+	var currTime = startTime;
+	var parsers = [];
+	var sound;
+	var audioAnalyser;
+	
+	var settings = {
+	  changeColor: true,
+	  bulgeSpeaker: false,
+	  varyIter: false,
+	  changeBackground: true
 	};
+	
+	var colorMat = new THREE.ShaderMaterial({
+	  uniforms: {
+	    image: { type: "t", value: THREE.ImageUtils.loadTexture('./colors.jpg') },
+	    u_albedo: { type: 'v3', value: new THREE.Color('#404040') },
+	    u_ambient: { type: 'v3', value: new THREE.Color('#404040') },
+	    u_lightPos: { type: 'v3', value: new THREE.Vector3(75, 100, 0) },
+	    u_lightCol: { type: 'v3', value: new THREE.Color('#ffffff') },
+	    u_lightIntensity: { type: 'f', value: 2 },
+	    loudness: { type: 'f', value: 0.0 }
+	  },
+	  vertexShader: __webpack_require__(10),
+	  fragmentShader: __webpack_require__(11)
+	});
 	
 	function onLoad(framework) {
 	  var scene = framework.scene,
@@ -73,24 +95,13 @@
 	
 	
 	  lightsCamera(scene, camera);
-	  showShapeGrammar(scene, camera);
 	  showLayout(scene, camera);
+	  addMusic(scene, camera);
 	
-	  gui.add(camera, 'fov', 0, 180).onChange(function (newVal) {
-	    camera.updateProjectionMatrix();
-	  });
-	
-	  gui.add(input, "shapeGrammar").onChange(function (newVal) {
-	    clearScene(scene, camera);
-	    showShapeGrammar(scene, camera);
-	    showLayout(scene, camera);
-	  });
-	
-	  gui.add(input, "layout").onChange(function (newVal) {
-	    clearScene(scene, camera);
-	    showShapeGrammar(scene, camera);
-	    showLayout(scene, camera);
-	  });
+	  gui.add(settings, 'changeColor');
+	  gui.add(settings, 'bulgeSpeaker');
+	  gui.add(settings, 'varyIter');
+	  gui.add(settings, 'changeBackground');
 	}
 	
 	function clearScene(scene, camera) {
@@ -102,10 +113,10 @@
 	}
 	
 	function lightsCamera(scene, camera) {
-	  camera.position.set(1, 100, 200);
+	  camera.position.set(0, 20, 0);
 	  camera.lookAt(new THREE.Vector3(0, 0, 0));
 	
-	  var light = new THREE.AmbientLight(0x404040); // soft white light
+	  var light = new THREE.AmbientLight(0x404040);
 	  scene.add(light);
 	
 	  var directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
@@ -114,86 +125,171 @@
 	}
 	
 	function showShapeGrammar(scene, camera) {
-	  if (input.shapeGrammar && !input.layout) {
-	    camera.position.set(1, 10, 10);
-	    camera.lookAt(new THREE.Vector3(0, 0, 0));
+	  camera.position.set(1, 10, 10);
+	  camera.lookAt(new THREE.Vector3(0, 0, 0));
 	
-	    var position = new THREE.Vector3(0, 0, 0);
-	    var direction = new THREE.Vector3(0, 1, 0);
-	    var parser = new _houseParser2.default(position, direction, scene, 4, 10);
-	    parser.render();
-	  }
+	  var position = new THREE.Vector3(0, 0, 0);
+	  var direction = new THREE.Vector3(0, 1, 0);
+	  var parser = new _houseParser2.default(position, direction, scene, 10);
+	  parser.render(1);
 	}
 	
 	function showLayout(scene, camera) {
-	  if (input.layout) {
-	    camera.position.set(1, 100, 200);
-	    camera.lookAt(new THREE.Vector3(0, 0, 0));
+	  camera.position.set(221, 10, 25);
+	  camera.lookAt(new THREE.Vector3(0, 0, 0));
 	
-	    var height = 100;
-	    var radius = 50;
+	  var height = 100;
+	  var radius = 50;
 	
-	    //speaker mid
-	    var spkMG = new THREE.CylinderGeometry(radius, radius, height, 32);
-	    var spkMM = new THREE.MeshLambertMaterial({ color: 0xa9a9a9 });
-	    var spkM = new THREE.Mesh(spkMG, spkMM);
-	    scene.add(spkM);
+	  //speaker mid
+	  var spkMG = new THREE.CylinderGeometry(radius, radius, height, 32);
+	  var spkMM = new THREE.MeshPhongMaterial({ color: 0x191919 });
+	  var spkM = new THREE.Mesh(spkMG, spkMM);
+	  scene.add(spkM);
 	
-	    //speaker top
-	    var spkTG = new THREE.CylinderGeometry(2 * radius / 3, radius, height / 4, 32);
-	    var spkTM = new THREE.MeshLambertMaterial({ color: 0xa9a9a9 });
-	    var spkT = new THREE.Mesh(spkTG, spkTM);
-	    spkT.position.set(0, 10 * height / 16, 0);
-	    scene.add(spkT);
+	  //speaker top
+	  var spkTG = new THREE.CylinderGeometry(2 * radius / 3, radius, height / 4, 32);
+	  var spkTM = new THREE.MeshPhongMaterial({ color: 0x191919 });
+	  var spkT = new THREE.Mesh(spkTG, spkTM);
+	  spkT.position.set(0, 10 * height / 16, 0);
+	  scene.add(spkT);
 	
-	    //speaker base
-	    var spkBG = new THREE.CylinderGeometry(radius, 2 * radius / 3, height / 4, 32);
-	    var spkBM = new THREE.MeshLambertMaterial({ color: 0xa9a9a9 });
-	    var spkB = new THREE.Mesh(spkBG, spkBM);
-	    spkB.position.set(0, -5 * height / 8, 0);
-	    scene.add(spkB);
+	  //speaker base
+	  var spkBG = new THREE.CylinderGeometry(radius, 2 * radius / 3, height / 4, 32);
+	  var spkBM = new THREE.MeshPhongMaterial({ color: 0x191919 });
+	  var spkB = new THREE.Mesh(spkBG, spkBM);
+	  spkB.position.set(0, -5 * height / 8, 0);
+	  scene.add(spkB);
 	
-	    for (var y = -(height / 2) + 5; y < height / 2; y += 5) {
-	      for (var x = 5 - radius; x < radius; x += 5) {
-	        var z = Math.sqrt(radius * radius - x * x);
-	        var dir = new THREE.Vector3(-x, y, -z);
-	        var dirO = new THREE.Vector3(-x, y, z);
+	  for (var y = 5 - height / 2; y <= height / 2 - 5; y += 5) {
+	    for (var a = 0; a < 2 * Math.PI; a += Math.PI / 27) {
+	      var x = radius * Math.sin(a);
+	      var z = radius * Math.cos(a);
+	      var dir = new THREE.Vector3(-x, y, -z);
 	
-	        var cg = new THREE.CylinderGeometry(2.5, 2.5, 2, 16);
-	        var m = new THREE.MeshLambertMaterial({ color: 0xffff00 });
-	        var cyl = new THREE.Mesh(cg, m);
+	      var cg = new THREE.CylinderGeometry(2.5, 2.5, 2, 16);
+	      var m = colorMat;
+	      var cyl = new THREE.Mesh(cg, m);
 	
-	        cyl.position.set(x, y, z);
-	        cyl.lookAt(dir);
-	        cyl.rotateX(Math.PI / 2);
-	        scene.add(cyl);
+	      cyl.position.set(x, y, z);
+	      cyl.lookAt(dir);
+	      cyl.rotateX(Math.PI / 2);
 	
-	        var cg2 = new THREE.CylinderGeometry(2.5, 2.5, 2, 16);
-	        var cyl2 = new THREE.Mesh(cg2, m);
+	      scene.add(cyl);
 	
-	        cyl2.position.set(x, y, -z);
-	        cyl2.lookAt(dirO);
-	        cyl2.rotateX(Math.PI / 2);
-	        scene.add(cyl2);
+	      var position = new THREE.Vector3(x, y, -z);
+	      dir.normalize();
+	      var dirS = new THREE.Vector3(x + dir.x, 0, -z + dir.z).normalize();
+	      position.addScaledVector(dirS, 1.0);
+	      var parser = new _houseParser2.default(position, dirS, scene, 2.5);
 	
-	        if (input.shapeGrammar) {
-	          var position = new THREE.Vector3(x, y, -z);
-	          var direction = dir.normalize();
-	          var parser = new _houseParser2.default(position, direction, scene, 4, 2.5);
+	      parsers.push(parser);
 	
-	          var position2 = new THREE.Vector3(x, y, z);
-	          var direction2 = dirO.normalize();
-	          var parser2 = new _houseParser2.default(position2, direction2, scene, 4, 2.5);
-	
-	          parser.render();
-	          parser2.render();
-	        }
-	      }
+	      parser.render(1);
 	    }
 	  }
 	}
 	
-	function onUpdate(framework) {}
+	function addMusic(scene, camera) {
+	  var listener = new THREE.AudioListener();
+	  camera.add(listener);
+	
+	  sound = new THREE.Audio(listener);
+	  var audioLoader = new THREE.AudioLoader();
+	
+	  audioLoader.load('../music.mp3', function (buffer) {
+	    sound.setBuffer(buffer);
+	    sound.setLoop(true);
+	    sound.setVolume(0.5);
+	    sound.play();
+	  });
+	
+	  audioAnalyser = new THREE.AudioAnalyser(sound, 64);
+	}
+	
+	function onUpdate(framework) {
+	  currTime = Date.now() - startTime;
+	
+	  var t = currTime / 10000;
+	
+	  if (t < 1.35) {
+	    rotateCam(framework.camera, 200, t, 2);
+	  } else if (t < 2) {
+	    settings.bulgeSpeaker = true;
+	    rotateCam(framework.camera, 100, t, 10);
+	  } else if (t < 2.1) {
+	    framework.camera.fov = 75;
+	    settings.varyIter = true;
+	  } else if (t < 3) {
+	    rotateCam(framework.camera, 100, t, 30);
+	  } else if (t < 5) {
+	    settings.bulgeSpeaker = false;
+	    framework.camera.fov = 75;
+	    rotateCam(framework.camera, 60, -t, 30);
+	  } else {
+	    framework.camera.position.set(221, 10, 25);
+	    framework.camera.lookAt(new THREE.Vector3(0, 0, 0));
+	    settings.bulgeSpeaker = true;
+	  }
+	  var r = Math.random();
+	
+	  if (audioAnalyser) {
+	    var avgFrequency = audioAnalyser.getAverageFrequency() / 128;
+	    r = clamp((avgFrequency - 0.60) / 0.4, 0, 1); //usually between 0.3 and 0.9
+	  }
+	
+	  // small cyls change color based on loudness
+	  if (settings.changeColor) {
+	    colorMat.uniforms.loudness.value = r * 4;
+	  }
+	
+	  // bulge speaker based on loudness
+	  if (settings.bulgeSpeaker) {
+	    framework.camera.fov = clamp(r * 120, 75, 100);
+	    framework.camera.updateProjectionMatrix();
+	  }
+	
+	  // vary iterations with loudness values
+	  if (settings.varyIter) {
+	    var iter = clamp(Math.ceil((r - 0.3) / 0.7 * 5), 1, 5);
+	    for (var i = 0; i < parsers.length; i++) {
+	      if (Math.random() < 0.5) {
+	        parsers[i].render(iter);
+	      }
+	    }
+	  }
+	
+	  //change background color when loudness above threshold
+	  if (settings.changeBackground) {
+	    framework.scene.background = getRandomColor(r);
+	  }
+	}
+	
+	function rotateCam(camera, radius, t, f) {
+	  var angle = t * (2 * Math.PI) / f;
+	  camera.position.set(radius * Math.cos(angle), 0, radius * Math.sin(angle));
+	  camera.lookAt(new THREE.Vector3(0, 0, 0));
+	  if (radius < 100) {
+	    camera.lookAt(new THREE.Vector3(camera.position.x / 2, radius, camera.position.z / 2));
+	  }
+	  camera.updateProjectionMatrix();
+	}
+	
+	function clamp(num, min, max) {
+	  return num <= min ? min : num >= max ? max : num;
+	}
+	
+	function getRandomColor(t) {
+	  var a = new THREE.Vector3(0.5, 0.5, 0.5);
+	  var b = new THREE.Vector3(0.5, 0.5, 0.5);
+	  var c = new THREE.Vector3(1.0, 1.0, 1.0);
+	  var d = new THREE.Vector3(0.00, 0.33, 0.67);
+	  return new THREE.Color(getC(t, a.x, b.x, c.x, d.x), getC(t, a.y, b.y, c.y, d.y), getC(t, a.z, b.z, c.z, d.z));
+	}
+	
+	function getC(t, a, b, c, d) {
+	  return a + b * Math.cos(6.28318 * (c * t + d));
+	}
 	
 	_framework2.default.init(onLoad, onUpdate);
 
@@ -48096,7 +48192,7 @@
 	};
 	
 	var Parser = function () {
-	    function Parser(position, direction, scene, iterations, radius) {
+	    function Parser(position, direction, scene, radius) {
 	        _classCallCheck(this, Parser);
 	
 	        this.initPos = position;
@@ -48106,7 +48202,12 @@
 	        this.stack = [];
 	
 	        this.scene = scene;
-	        this.totalIter = iterations;
+	
+	        this.house = new _houseLsystem2.default();
+	
+	        this.objs = [];
+	
+	        this.idxCtr = 0;
 	
 	        this.renderGrammar = {
 	            'L': this.addLevel.bind(this),
@@ -48124,6 +48225,8 @@
 	        };
 	
 	        this.stdDir = [new THREE.Vector3(-1, 0, 1).normalize(), new THREE.Vector3(0, 0, 1), new THREE.Vector3(1, 0, 1).normalize(), new THREE.Vector3(1, 0, 0), new THREE.Vector3(1, 0, -1).normalize(), new THREE.Vector3(0, 0, -1), new THREE.Vector3(-1, 0, -1).normalize(), new THREE.Vector3(-1, 0, 0)];
+	
+	        this.renderSymbols(this.house.doIterations(5));
 	    }
 	
 	    _createClass(Parser, [{
@@ -48188,7 +48291,9 @@
 	            cyl.lookAt(pos.addScaledVector(dir, height / 2));
 	            cyl.rotateX(Math.PI / 2);
 	
-	            this.scene.add(cyl);
+	            this.objs.push({ index: this.currIdx, object: cyl });
+	
+	            //this.scene.add(cyl);
 	        }
 	    }, {
 	        key: 'addBox',
@@ -48215,7 +48320,9 @@
 	            box.lookAt(pos.addScaledVector(dir, height / 2));
 	            box.rotateX(Math.PI / 2);
 	
-	            this.scene.add(box);
+	            this.objs.push({ index: this.currIdx, object: box });
+	
+	            //this.scene.add(box);
 	        }
 	    }, {
 	        key: 'renderSymbol',
@@ -48235,14 +48342,31 @@
 	            this.currIdx = 0;
 	            for (var currentNode = linkedList.head; currentNode != null; currentNode = currentNode.next) {
 	                this.renderSymbol(currentNode);
-	                this.currIdx++;
+	                this.currIdx = currentNode.iter;
 	            }
 	        }
 	    }, {
 	        key: 'render',
-	        value: function render() {
-	            var house = new _houseLsystem2.default();
-	            this.renderSymbols(house.doIterations(this.totalIter));
+	        value: function render(iterations) {
+	
+	            if (iterations > this.idxCtr) {
+	                for (var i = this.idxCtr; i < iterations; i++) {
+	                    for (var j = 0; j < this.objs.length; j++) {
+	                        if (this.objs[j].index == i) {
+	                            this.scene.add(this.objs[j].object);
+	                        }
+	                    }
+	                }
+	            } else if (iterations < this.idxCtr) {
+	                for (var i = this.idxCtr; i >= iterations; i--) {
+	                    for (var j = 0; j < this.objs.length; j++) {
+	                        if (this.objs[j].index == i) {
+	                            this.scene.remove(this.objs[j].object);
+	                        }
+	                    }
+	                }
+	            }
+	            this.idxCtr = iterations;
 	        }
 	    }]);
 	
@@ -48405,9 +48529,22 @@
 	                currentNode = next;
 	            }
 	        }
+	        this.axiom = linkedListToString(lSystemLL);
 	        return lSystemLL;
 	    };
 	}
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports) {
+
+	module.exports = "varying float noise;\nuniform float amplitude;\nuniform float loudness;\nuniform vec3 inclination;\nfloat M_PI = 3.14159265359;\n\nvarying vec3 f_position;\nvarying vec3 f_normal;\n\nfloat noise_gen(vec3 pos)\n{\n  return fract(sin(dot(pos, vec3(12.9898, 78.233, 39.73))) * 43758.545);\n}\n\nfloat lerp(float a, float b, float t)\n{\n  return a * (1.0 - t) + b * t;\n}\n\nfloat cerp(float a, float b, float t) {\n  float cos_t = (1.0 - cos(t * M_PI)) * 0.5;\n  return a * (1.0 - cos_t) + b * cos_t;\n}\n\nfloat smooth(vec3 pos, float fq)\n{\n\tfloat r = 1.0 / fq;\n\treturn (noise_gen(pos + vec3(r, r, r)) + noise_gen(pos + vec3(- r, r, r))\n\t\t+ noise_gen(pos + vec3(r,- r, r)) + noise_gen(pos + vec3(- r,- r, r))\n\t\t+ noise_gen(pos + vec3(r, r,- r)) + noise_gen(pos + vec3(- r, r,- r))\n\t\t+ noise_gen(pos + vec3(r, - r,- r)) + noise_gen(pos + vec3(- r,- r,- r))) / 8.0;\n}\n\nfloat noise_interpolate(vec3 pos, float fq)\n{\n  pos *= 0.2;\n\n  vec3 a = vec3(floor(pos.x), ceil(pos.y), ceil(pos.z));\n  vec3 b = vec3(ceil(pos.x), ceil(pos.y), ceil(pos.z));\n  vec3 c = vec3(floor(pos.x), floor(pos.y), ceil(pos.z));\n  vec3 d = vec3(ceil(pos.x), floor(pos.y), ceil(pos.z));\n\n  vec3 e = vec3(floor(pos.x), ceil(pos.y), floor(pos.z));\n  vec3 f = vec3(ceil(pos.x), ceil(pos.y), floor(pos.z));\n  vec3 g = vec3(floor(pos.x), floor(pos.y), floor(pos.z));\n  vec3 h = vec3(ceil(pos.x), floor(pos.y), floor(pos.z));\n\n  float ab = cerp(smooth(a, fq), smooth(b, fq), fract(pos.x));\n  float cd = cerp(smooth(c, fq), smooth(d, fq), fract(pos.x));\n  float abcd = cerp(cd, ab, fract(pos.y));\n\n  float ef = cerp(smooth(e, fq), smooth(f, fq), fract(pos.x));\n  float gh = cerp(smooth(g, fq), smooth(h, fq), fract(pos.x));\n  float efgh = cerp(gh, ef, fract(pos.y));\n\n  return cerp(efgh, abcd, fract(pos.z));\n}\n\nfloat pnoise(vec3 pos)\n{\n\tfloat total = 0.0;\n\n\tfor (int i = 0; i < 16; ++i)\n\t{\n\t\tfloat fq = pow(2.0, float(i));\n\t\tfloat amplitude = pow(0.59, float(i));\n\n\t\ttotal += noise_interpolate(pos, fq) * amplitude;\n\t}\n\treturn total;\n}\n\nvoid main() {\n\tf_normal = normal;\n    f_position = position;\n\tnoise = pnoise(position + vec3(loudness, loudness, loudness)) - 0.6;\n\tgl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\n}"
+
+/***/ }),
+/* 11 */
+/***/ (function(module, exports) {
+
+	module.exports = "uniform vec3 u_albedo;\nuniform vec3 u_ambient;\nuniform vec3 u_lightPos;\nuniform vec3 u_lightCol;\nuniform float u_lightIntensity;\nuniform float time;\n\n\nvarying float noise;\n\nuniform sampler2D image;\n\nvarying vec3 f_position;\nvarying vec3 f_normal;\n\nvoid main() {\n    vec4 color = texture2D( image, vec2(1, noise));\n    \n    vec3 norm = f_normal.xyz;\n    norm.x *= - 1.0;\n    norm.y *= - 1.0;\n    norm.z *= - 1.0;\n    float d = clamp(dot(norm, normalize(u_lightPos - f_position)), 0.0, 1.0);\n\n    gl_FragColor = vec4(d * color.rgb * u_lightCol * u_lightIntensity + u_ambient, 1.0);\n}"
 
 /***/ })
 /******/ ]);
